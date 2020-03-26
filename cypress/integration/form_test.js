c08445478948974414e6064800f15995c10983df
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-26 16:04:05 -0400

    cypress installed

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,8 @@
+describe("Test our inputs and submit our form",function(){
+    beforeEach(function({
+    cy.visit("http://localhost:3000/");
+
+});
+    it("Add text to inputs and submit form", function(){
+    });
+});
